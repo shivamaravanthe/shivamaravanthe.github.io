2973c6aae963332ad50400cc309b6f76139f8987
shivamaravanthe <vshivanand2@gmail.com>
2020-05-05 20:07:54 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function myFunction1() {
+    document.getElementById("frameDiv").style.display = "block";
+    document.getElementById("xmark").style.display ="block";
+}
+function myFunction2(){
+    document.getElementById("frameDiv").style.display = "none";
+    document.getElementById("xmark").style.display="none";
+}
