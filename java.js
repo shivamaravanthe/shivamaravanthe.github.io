2e527d8d35932993121cce109ec8fda876276691
shivamaravanthe <vshivanand2@gmail.com>
2020-05-05 19:59:19 +0530

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-function myFunction1() {
-    document.getElementById("abcdrt").style.display = "block";
-    document.getElementById("xmark").style.display ="block";
-}
-function myFunction2(){
-    document.getElementById("abcdrt").style.display = "none";
-    document.getElementById("xmark").style.display="none";
-}
