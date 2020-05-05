d3920ccda4f159ea839f2e7c816505def005df5f
shivamaravanthe <vshivanand2@gmail.com>
2020-05-05 19:50:02 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function myFunction1() {
+    document.getElementById("abcdrt").style.display = "block";
+    document.getElementById("xmark").style.display ="block";
+}
+function myFunction2(){
+    document.getElementById("abcdrt").style.display = "none";
+    document.getElementById("xmark").style.display="none";
+}
