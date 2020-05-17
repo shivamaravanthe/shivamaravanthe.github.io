a18e17bac610d10b301e3b3b24fa41e8b03dd8a0
shivamaravanthe <vshivanand2@gmail.com>
2020-05-17 12:08:01 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,35 @@
-function test() {
+function clean() {
+  localStorage.clear();
+}
+function showItems() {
+var retrievedData=localStorage.getItem("AddedDirectly");
+array=JSON.parse(retrievedData);
+if(array==null)array=["0"];
+for (var i = 1; i <=array.length; i++) {
+  var li = document.createElement("li");
+  var inputValue = array[i]
+  if(inputValue==null)continue;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  document.getElementById("myUL").appendChild(li);
+  //document.getElementById("myInput").value = "";
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  for (var j = 0; j < close.length; j++) {
+    close[j].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
 for (var i = 1; i < 10; i++) {
 var li = document.createElement("li");
 var inputValue = localStorage.getItem(i);
-if(inputValue==null)break;
+if(inputValue==null)continue;
 var t = document.createTextNode(inputValue);
 li.appendChild(t);
 document.getElementById("myUL").appendChild(li);
@@ -43,14 +70,27 @@ for (i = 0; i < close.length; i++) {
 }
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
+  var array=["0"];
+  var alpha = /^[A-Za-z]+$/;
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  var prodname = document.getElementById("prodname").value;
+  var text=prodname+" "+inputValue;
+  var li = document.createElement("li");
+  var t = document.createTextNode(text);
   li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
+  if (prodname == '') {
+    alert("ಎಂತ ಬೇಕು ಅಂತ  ಹಾಕಿಲ್ಲ");
+  }
+  else if (inputValue === '') {
+    alert(prodname+" "+"ಎಷ್ಟು ಬೇಕು ಅಂತ ಹಾಕ್..");
+  }
+  else {
     document.getElementById("myUL").appendChild(li);
+    var retrievedData=localStorage.getItem("AddedDirectly");
+    array=JSON.parse(retrievedData);
+    if(array==null)array=["0"];
+    array.push(text);
+    localStorage.setItem("AddedDirectly",JSON.stringify(array));
   }
   document.getElementById("myInput").value = "";
 
@@ -59,7 +99,7 @@ function newElement() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
+  //alert(array.length);
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
@@ -102,7 +142,7 @@ function intialize() {
   ) {
      // is Google Chrome
   } else {
-     alert("pls open in Google Chrome"); 
+     alert("pls open in Google Chrome");
   }
 }
 function displaycontent(a) {
@@ -146,4 +186,14 @@ function submit(a) {
 function fadeOut() {
   var slideSource = document.getElementById('bag');
   slideSource.classList.toggle('fade');
+}
+function checknamew() {
+  var name=document.getElementById("custname").value;
+  if(name=='')alert("'ನಿಮ್ಮ ಹೆಸರ'ನ್ನು ನಮೂದಿಸಿ");
+  else window.open('https://wa.me/918660537925?text=Im%20interested%20in%20your%20car%20for%20sale');
+}
+function checknamee() {
+  var name=document.getElementById("custname").value;
+  if(name=='')alert("'ನಿಮ್ಮ ಹೆಸರ'ನ್ನು ನಮೂದಿಸಿ");
+  else window.open('mailto:somepoorswine@poomail.poo?Subject=Summat%20or%20Other&body=I%20Have%20been%20%20%20for%20a%20massive%20%20%20%20%20%20%20poo');
 }
