0f0145df0e492f990c13aeab7b8d8976025796ab
shivamaravanthe <vshivanand2@gmail.com>
2020-07-20 19:30:14 +0530

    Update java.js

--- java.js
+++ java.js
@@ -3,9 +3,7 @@ function newElement() {
   var array=["0"];
   var inputValue = document.getElementById("myInput").value;
   var prodname = document.getElementById("prodname").value;
-  var spaceafp=25;
-  var spaceafiV=15;
-  var text=prodname+Array(spaceafp).fill('\xa0').join('')+inputValue+Array(spaceafiV).fill('\xa0').join('')+'₹--';
+  var text=prodname+Array(25).fill('\xa0').join('')+inputValue+Array(15).fill('\xa0').join('')+'₹--';
   var li = document.createElement("li");
   var t = document.createTextNode(text);
   li.appendChild(t);
@@ -26,80 +24,19 @@ function newElement() {
   document.getElementById("myInput").value = "";
   document.getElementById("prodname").value="";
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var txt = document.createTextNode("X");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-  var close = document.getElementsByClassName("close");
-  for (var j = 0; j < close.length; j++) {
-    close[j].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-      var del=div.innerHTML;
-      del=del.toString();
-      del=del.substring(0, del.length-28);
-      var retrievedData=localStorage.getItem("AddedDirectly");
-      array=JSON.parse(retrievedData);
-      if(array==null||array=='')array=['0'];
-      var index = array.indexOf(del);
-      if (index > -1) {
-        array.splice(index, 1);
-      }
-      localStorage.setItem("AddedDirectly",JSON.stringify(array));
-      del=del.toString();
-      del=del.substring(del.length - 1,del.length - 2);
-      for (var i = del; i <=del; i++) {
-        var inputValue = localStorage.getItem(i);
-        if(inputValue==null)continue;
-        inputValue=inputValue.toString();
-        inputValue=inputValue.substring(inputValue.length - 1,inputValue.length);
-        if(inputValue==del)localStorage.removeItem(del);
+  CalculateTotal();
+  Gohere();
 
-      }
-
-      }
-    }
-    /*Calculate Total*/
-    var x=document.getElementById('myUL');
-    str = x.innerHTML;
-    var array=[];
-    var open=0,amp=0,text='';
-    for (var i = 0; i < str.length; i++){
-      if(str[i]=='<'){open=1;}
-      if(str[i]=='&'){amp=1;}
-      if(open==0&&amp==0){text=text+str[i];}
-      if(str[i]=='>'){open=0;}
-      if(amp==1){if(!(isNaN(str[i+1]))){amp=0;text=text+'₹';}else if (str[i+1] == '₹') {amp=0;}}
-      }
-      var number='',snum=0;
-      var totalP=0;
-      open=0,amp=0;
-    for (var i = 3; i < text.length; i++ ){
-      if(text[i]=="×"&&amp==2){
-        amp=0;
-        open=1;
-        snum=0;
-        if(number!='')
-        totalP=totalP+parseFloat(number);
-        number='';
-      }
-      if(text[i]=="₹"&&amp==1){
-        amp=2;snum=1;
-      }
-      if(text[i]=="₹"&&amp==0){
-        amp=1;
-        }
-        if(text[i]!='-'&&text[i]!="₹"&&snum==1){
-        number=number+text[i];
-      }
-      if(open=1){open=0;}
-    }
-    document.getElementById("GrandTotal").innerHTML='Total:-'+totalP;
 }
 function showItems() {
+
     var retrievedData=localStorage.getItem("AddedDirectly");
     array=JSON.parse(retrievedData);
-    if(array==null)array=[''];
+    if(array==null)array=['0'];
     for (var i = 0; i <=array.length; i++) {
     var li = document.createElement("li");
     var inputValue = array[i];
@@ -110,124 +47,15 @@ function showItems() {
     document.getElementById("myInput").value = "";
     document.getElementById("prodname").value="";
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var txt = document.createTextNode("X");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-    var close = document.getElementsByClassName("close");
-    for (var j = 0; j < close.length; j++) {
-      close[j].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-        var del=div.innerHTML;
-        del=del.toString();
-        del=del.substring(0, del.length - 28);
-        var retrievedData=localStorage.getItem("AddedDirectly");
-        array=JSON.parse(retrievedData);
-        if(array==null||array=='')array=['0'];
-        var index = array.indexOf(del);
-        if(array==null||array=='')index = -1;
-        if (index > -1) {
-          array.splice(index, 1);
-        }
-        localStorage.setItem("AddedDirectly",JSON.stringify(array));
-        del=del.toString();
-        del=del.substring(del.length - 1,del.length - 2);
-        for (var i = del; i <=del; i++) {
-          var inputValue = localStorage.getItem(i);
-          if(inputValue==null)continue;
-          inputValue=inputValue.toString();
-          inputValue=inputValue.substring(inputValue.length - 1,inputValue.length);
-          if(inputValue==del)localStorage.removeItem(del);
-
-        }
-        }
-      }
-
-
     }
-
-    for (var i = 1; i < 10; i++) {
-    var li = document.createElement("li");
-    var inputValue = localStorage.getItem(i);
-      if(inputValue==null)continue;
-      var t = document.createTextNode(inputValue);
-      li.appendChild(t);
-      document.getElementById("myUL").appendChild(li);
-      document.getElementById("myInput").value = "";
-      var span = document.createElement("SPAN");
-      var txt = document.createTextNode("\u00D7");
-      span.className = "close";
-      span.appendChild(txt);
-      li.appendChild(span);
-      var close = document.getElementsByClassName("close");
-      for (var j = 0; j < close.length; j++) {
-        close[j].onclick = function() {
-          var div = this.parentElement;
-          div.style.display = "none";
-          var del=div.innerHTML;
-          del=del.toString();
-          del=del.substring(0, del.length - 28);
-          var retrievedData=localStorage.getItem("AddedDirectly");
-          array=JSON.parse(retrievedData);
-          if(array==null||array=='')array=['0'];
-          var index = array.indexOf(del);
-          if(array==null||array=='')index = -1;
-          if (index > -1) {
-            array.splice(index, 1);
-          }
-          localStorage.setItem("AddedDirectly",JSON.stringify(array));
-          del=del.toString();
-          del=del.substring(del.length - 1,del.length - 2);
-          for (var i = del; i <=del; i++) {
-            var inputValue = localStorage.getItem(i);
-            if(inputValue==null)continue;
-            inputValue=inputValue.toString();
-            inputValue=inputValue.substring(inputValue.length - 1,inputValue.length);
-            if(inputValue==del)localStorage.removeItem(del);
-          }
-console.log(del);
-          }
-        }
-      }
-      /*Calculate Total*/
-      var x=document.getElementById('myUL');
-      str = x.innerHTML;
-      var array=[];
-      var open=0,amp=0,text='';
-      for (var i = 0; i < str.length; i++){
-        if(str[i]=='<'){open=1;}
-        if(str[i]=='&'){amp=1;}
-        if(open==0&&amp==0){text=text+str[i];}
-        if(str[i]=='>'){open=0;}
-        if(amp==1){if(!(isNaN(str[i+1]))){amp=0;text=text+'₹';}else if (str[i+1] == '₹') {amp=0;}}
-        }
-        var number='',snum=0;
-        var totalP=0;
-        open=0,amp=0;
-      for (var i = 3; i < text.length; i++ ){
-        if(text[i]=="×"&&amp==2){
-          amp=0;
-          open=1;
-          snum=0;
-          if(number!='')
-          totalP=totalP+parseFloat(number);
-          number='';
-        }
-        if(text[i]=="₹"&&amp==1){
-          amp=2;snum=1;
-        }
-        if(text[i]=="₹"&&amp==0){
-          amp=1;
-          }
-          if(text[i]!='-'&&text[i]!="₹"&&snum==1){
-          number=number+text[i];
-        }
-        if(open=1){open=0;}
-      }
-      document.getElementById("GrandTotal").innerHTML='Total:-'+totalP;
-   //console.log(totalP);
-    }
+    /*Calculate Total*/
+    CalculateTotal();
+    Gohere();
+}
 function calprice(p,j) {
   var x = document.querySelectorAll("p");
   var y = document.querySelectorAll("input");
@@ -244,10 +72,13 @@ function submit(a) {
   price=price[a-1].innerHTML;
   var quantity=document.getElementsByName("quantity");
   quantity=quantity[a-1].value;
-  var  fpspace = 25;
-  var fqspace = 15;
-  var value=name+Array(fpspace).fill('\xa0').join('')+quantity+Array(fqspace).fill('\xa0').join('')+price;
-  localStorage.setItem(a,value);
+  var value=name+Array(25).fill('\xa0').join('')+quantity+Array(15).fill('\xa0').join('')+price;
+  var array=["0"];
+  var retrievedData=localStorage.getItem("AddedDirectly");
+  array=JSON.parse(retrievedData);
+  if(array==null)array=["0"];
+  array.push(value);
+  localStorage.setItem("AddedDirectly",JSON.stringify(array));
   var price=document.getElementsByClassName("price");
   price[a-1].innerHTML="₹0";
   var x = document.querySelectorAll("p");
@@ -255,7 +86,7 @@ function submit(a) {
   var z = x.length-1;
   for (var i = 1; i <= z ; i++) {
     if(i==a)
-        y[i-1].value=0;
+        y[i-1].value='';
   }
   var slideSource = document.getElementById('bag');
   slideSource.classList.toggle('fade');
@@ -266,87 +97,21 @@ function fadeOut() {
   slideSource.classList.toggle('fade');
 }
 function checknamew() {
-  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  var today  = new Date();
   var name=document.getElementById("custname").value;
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var x=document.getElementById('myUL');
-  str = x.innerHTML;
-  var array=[];
-  var open=0,amp=0,text='';
-  for (var i = 0; i < str.length; i++){
-    if(str[i]=='<'){open=1;}
-    if(str[i]=='&'){amp=1;}
-    if(open==0&&amp==0){text=text+str[i];}
-    if(str[i]=='>'){open=0;}
-    if(amp==1){if(!(isNaN(str[i+1]))){amp=0;text=text+'₹';}else if (str[i+1] == '₹') {amp=0;}}
-    }
-    var formated="Name:"+name+"%09%09%09%09"+today.toLocaleDateString("en-US", options)+"%0A%0A";
-    open=0,amp=0;
-  for (var i = 3; i < text.length; i++ ){
-    if(text[i]=="×"&&amp==2){
-      amp=0;
-      open=1;
-      formated=formated+"%0A";
-    }
-    if(text[i]=="₹"&&amp==1){
-      amp=2;
-      formated=formated+"%20%20%20%20"+'₹';
-    }
-    if(text[i]=="₹"&&amp==0){
-      amp=1;
-      formated=formated+"%20%20%20%20";
+  if(name=='')alert("ನಿಮ್ಮ ಹೆಸರನ್ನು ನಮೂದಿಸಿ");
+    else{
+    var formated=BilltoSend();
+    window.open('https://wa.me/918660537925?text='+formated);
       }
-    if(text[i]!="₹"&&open!=1){
-      formated=formated+text[i];
-    }
-    if(open=1){open=0;}
-  }
-  if(name=='')alert("'ನಿಮ್ಮ ಹೆಸರ'ನ್ನು ನಮೂದಿಸಿ");
-  else window.open('https://wa.me/918660537925?text='+formated);
 }
 function checknamee() {
-  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  var today  = new Date();
-  var name=document.getElementById("custname").value;
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var x=document.getElementById('myUL');
-  str = x.innerHTML;
-  var array=[];
-  var open=0,amp=0,text='';
-  for (var i = 0; i < str.length; i++){
-    if(str[i]=='<'){open=1;}
-    if(str[i]=='&'){amp=1;}
-    if(open==0&&amp==0){text=text+str[i];}
-    if(str[i]=='>'){open=0;}
-    if(amp==1){if(!(isNaN(str[i+1]))){amp=0;text=text+'₹';}else if (str[i+1] == '₹') {amp=0;}}
-    }
-    var formated="Name:"+name+"%09%09%09%09"+today.toLocaleDateString("en-US", options)+"%0A%0A";
-    open=0,amp=0;
-  for (var i = 3; i < text.length; i++ ){
-    if(text[i]=="×"&&amp==2){
-      amp=0;
-      open=1;
-      formated=formated+"%0A";
-    }
-    if(text[i]=="₹"&&amp==1){
-      amp=2;
-      formated=formated+"%20%20%20%20"+'₹';
-    }
-    if(text[i]=="₹"&&amp==0){
-      amp=1;
-      formated=formated+"%20%20%20%20";
-      }
-    if(text[i]!="₹"&&open!=1){
-      formated=formated+text[i];
-    }
-    if(open=1){open=0;}
-  }
-  var name=document.getElementById("custname").value;
-  if(name=='')alert("'ನಿಮ್ಮ ಹೆಸರ'ನ್ನು ನಮೂದಿಸಿ");
-  else window.open('mailto:vshivanand2@gmail.com?Subject=Bill&body='+formated);
+   var name=document.getElementById("custname").value;
+   if(name=='')alert("ನಿಮ್ಮ ಹೆಸರನ್ನು ನಮೂದಿಸಿ");
+   else{
+     var formated=BilltoSend();
+     window.open('mailto:vshivanand2@gmail.com?Subject=Bill&body='+formated);
+     }
+
 }
 function displaycontent(a){
   var content=document.getElementById(a);
@@ -364,39 +129,149 @@ for (var j = 1; j <=document.getElementsByClassName("card").length; j++) {
   }
 }
 }
-function intialize() {
-  if (typeof(Storage) !== "undefined") {
-  } else {
-     alert("PLEASE open in Google Chrome");
-  }
-  // please note,
-  // that IE11 now returns undefined again for window.chrome
-  // and new Opera 30 outputs true for window.chrome
-  // but needs to check if window.opr is not undefined
-  // and new IE Edge outputs to true now for window.chrome
-  // and if not iOS Chrome check
-  // so use the below updated condition
-  var isChromium = window.chrome;
-  var winNav = window.navigator;
-  var vendorName = winNav.vendor;
-  var isOpera = typeof window.opr !== "undefined";
-  var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
-  var isIOSChrome = winNav.userAgent.match("CriOS");
+function intialize(){
+   // please note,
+   // that IE11 now returns undefined again for window.chrome
+   // and new Opera 30 outputs true for window.chrome
+   // but needs to check if window.opr is not undefined
+   // and new IE Edge outputs to true now for window.chrome
+   // and if not iOS Chrome check
+   // so use the below updated condition
+    var isChromium = window.chrome;
+   var winNav = window.navigator;
+   var vendorName = winNav.vendor;
+   var isOpera = typeof window.opr !== "undefined";
+   var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+   var isIOSChrome = winNav.userAgent.match("CriOS");
 
-  if (isIOSChrome) {
-     // is Google Chrome on IOS
-  } else if(
-    isChromium !== null &&
-    typeof isChromium !== "undefined" &&
-    vendorName === "Google Inc." &&
-    isOpera === false &&
-    isIEedge === false
-  ) {
-     // is Google Chrome
-  } else {
-     alert("pls open in Google Chrome");
-  }
+   if (isIOSChrome){}// is Google Chrome on IOS
+    else if(isChromium !== null && typeof isChromium !== "undefined" && vendorName === "Google Inc." && isOpera === false && isIEedge === false){}
+    else {
+          alert("Best View is Obtained in Google Chrome");
+    }
 }
 function clean() {
   localStorage.clear();
+}
+function BilltoSend(){
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  var today  = new Date();
+  var name=document.getElementById("custname").value;
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  var x=document.getElementById('myUL');
+  str = x.innerHTML;
+  var array=[];
+  var open=0,amp=0,text='';
+  for (var i = 0; i < str.length; i++){
+    if(str[i]=='<'){open=1;}
+    if(str[i]=='&'){amp=1;}
+    if(open==0&&amp==0){text=text+str[i];}
+    if(str[i]=='>'){open=0;}
+    if(amp==1){if(!(isNaN(str[i+1]))){amp=0;text=text+'₹';}else if (str[i+1] == '₹') {amp=0;}}
+    }
+    var formated="Name:"+name+"%09%09%09%09"+today.toLocaleDateString("en-US", options)+"%0A%0A";
+    open=0,amp=0;
+  for (var i = 3; i < text.length; i++ ){
+    if(text[i]=="X"&&amp==2){
+      amp=0;
+      open=1;
+      formated=formated+"%0A";
+    }
+    if(text[i]=="₹"&&amp==1){
+      amp=2;
+      formated=formated+"%20%20%20%20"+'₹';
+    }
+    if(text[i]=="₹"&&amp==0){
+      amp=1;
+      formated=formated+"%20%20%20%20";
+      }
+    if(text[i]!="₹"&&open!=1){
+      formated=formated+text[i];
+    }
+    if(open=1){open=0;}
+  }
+  return formated;
+}
+function CalculateTotal(){
+  var x=document.getElementById('myUL');
+  str = x.innerHTML;
+  var array=[];
+  var open=0,amp=0,text='';
+  for (var i = 0; i < str.length; i++){
+    if(str[i]=='<'){open=1;}
+    if(str[i]=='&'){amp=1;}
+    if(open==0&&amp==0){text=text+str[i];}
+    if(str[i]=='>'){open=0;}
+    if(amp==1){if(!(isNaN(str[i+1]))){amp=0;text=text+'₹';}else if (str[i+1] == '₹') {amp=0;}}
+    }
+    var number='',snum=0;
+    var totalP=0;
+    open=0,amp=0;
+  for (var i = 3; i < text.length; i++ ){
+    if(text[i]=="X"&&amp==2){
+      amp=0;
+      open=1;
+      snum=0;
+      if(number!='')
+      totalP=totalP+parseFloat(number);
+      number='';
+    }
+    if(text[i]=="₹"&&amp==1){
+      amp=2;snum=1;
+    }
+    if(text[i]=="₹"&&amp==0){
+      amp=1;
+      }
+      if(text[i]!='-'&&text[i]!="₹"&&snum==1){
+      number=number+text[i];
+    }
+    if(open=1){open=0;}
+  }
+  document.getElementById("GrandTotal").innerHTML='Total:-'+totalP;
+}
+function Gohere() {
+  var close = document.getElementsByClassName("close");
+  for (var j = 0; j < close.length; j++) {
+    close[j].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+      var del=div.innerHTML;
+      del=del.toString();
+      var delarray=[]
+      delarray=del.split('<span class="close">X</span>');
+      del=delarray[0];
+      var textWithNBSpaceReplaced = del.replace(/&nbsp;/g, ' ');
+      var retrievedData=localStorage.getItem("AddedDirectly");
+      array=JSON.parse(retrievedData);
+      if(array==null||array=='')array=['0'];
+      var cd=0;
+      textWithNBSpaceReplaced = textWithNBSpaceReplaced.split("");
+      var textLen = textWithNBSpaceReplaced.length;
+      for(var i = 0 ; i<array.length; i++ ){
+         if(textLen == array[i].length){
+            for(var j = 0 ; j < textWithNBSpaceReplaced.length ; j++){console.log(array[i][j],textWithNBSpaceReplaced[j]);
+              if( textWithNBSpaceReplaced[j] == array[i][j] || textWithNBSpaceReplaced[j].charCodeAt() == 32 ){
+              cd = i; if(j == textWithNBSpaceReplaced.length-1){break;i=array.length;}}
+              else{cd='';}
+            }
+            }
+
+      }
+      console.log(cd);
+    localStorage.removeItem("AddedDirectly");
+    if(array==null||array=='')index = -1;
+    const index = array.indexOf(array[cd]);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+    localStorage.setItem("AddedDirectly",JSON.stringify(array));
+    var close = document.getElementById("myUL");
+    close.style.display = "none";
+    if(close.style.display=="none"){
+      location.reload();
+      close.style.display = "block";}
+    //showItems();
+      }
+    }
 }
